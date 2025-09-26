aa191dadfb29d55e566aee74fa73e4c0ab1df12a
ridu101 <ridu116540@gmail.com>
2025-09-26 12:25:36 +0600

    29-8 done

---
+++ seal.js
@@ -0,0 +1,15 @@
+const employee ={
+    name:'ridwan',
+    age:23,
+    position:'developer',
+    salary:5000
+
+}
+
+Object.seal(employee);
+
+delete employee.age
+
+employee.location='Nigeria';
+employee.experience='2 years';
+console.log(employee);
