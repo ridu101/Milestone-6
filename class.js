d6cde3d0fd9e23bba229ac5725e047cad9aa1b48
ridu101 <ridu116540@gmail.com>
2025-09-27 22:15:13 +0600

    30-5 done

---
+++ class.js
@@ -0,0 +1,17 @@
+  class Player {
+
+    constructor (name, age){
+        this.name= name;
+        this.age= age;
+        this.location= 'Dhaka';
+        this.language= 'JavaScript';
+    }
+
+  }
+
+  const player1= new Player('Ridu', 23)
+  const player2= new Player('Sidu', 24)
+  const player3= new Player('Lidu', 25)
+  const player4= new Player('Bidu', 26)
+  const player5= new Player('Midu', 27)     
+  console.log(player1, player2, player3, player4, player5);
