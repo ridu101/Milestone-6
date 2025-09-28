3c062592b6264d9487d0d1c48e9d858d8dfe4290
ridu101 <ridu116540@gmail.com>
2025-09-28 09:22:43 +0600

    for each related problem done

---
+++ Practise/for-each.js
@@ -0,0 +1,20 @@
+// Print each element of [1, 2, 3, 4, 5] multiplied by 2 (but don’t return a new array).
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach( c => console.log( c*2));
+
+// From ["red", "green", "blue"], print each color with the message: "Color: <name>" with it index like color 1: red.
+
+const colors = ["red", "green", "blue"];
+// colors.forEach( color => console.log( ` Color: ${color}`));
+
+colors.forEach( (color, index) => console.log( ` Color ${index + 1}: ${color}`));
+
+// From [100, 200, 300], calculate the sum of all numbers (using forEach).
+
+const nums = [100, 200, 300];
+let sum = 0;
+
+nums.forEach( n => sum = sum+n);
+console.log(sum);
